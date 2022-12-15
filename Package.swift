476a041009c306789804f05eba1334552106fb1f
Marcel Eschmann <marcel.eschmann@gmail.com>
2022-12-15 10:40:21 +0100

    Bump ios version

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ import PackageDescription
 let package = Package(
     name: "PixelKit",
     platforms: [
-        .iOS(.v11),
+        .iOS(.v13),
         .macOS(.v10_13),
         .tvOS(.v11)
     ],
